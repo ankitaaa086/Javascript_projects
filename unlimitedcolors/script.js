d77cb8e54e57aa9153ff920cdbcdd7c77ec7cf69
Ankita Jaiswal <ankitajaiswal7692@gmail.com>
2026-02-18 22:34:45 +0530

    Add files via upload

---
+++ unlimitedcolors/script.js
@@ -0,0 +1,32 @@
+// generate a random color
+
+const randomColor = function (){
+    const hex =  "0123456789ABCDEF";
+    let color = '#'
+
+    for(let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// console.log(randomColor()); 
+
+let intervalid;
+const startChangingColor = function(){
+    if(!intervalid)
+        intervalid = setInterval(changeBgColor, 1000);
+
+    function changeBgColor (){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+
+const stopChangingColor = function(){
+    clearInterval(intervalid);
+    intervalid = null;
+};
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
